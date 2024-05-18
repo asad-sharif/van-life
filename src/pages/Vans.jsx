881dec9e3b6405a4999ddfbe16c238897ef73222
Asad Sharif <asadsharif1270@gmail.com>
2024-05-18 15:26:35 +0500

    vans listed

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,5 +1,40 @@
-export default function Vans() { 
+import React from "react";
+import '../server'
+
+export default function Vans() {
+    const [vans, setVans] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("/api/vans")
+            .then(res => res.json())
+            .then(data => setVans(data.vans))
+    }, [])
+
+
+    let vanCard = vans.map(van => (
+        <div key={van.id} className="van-card">
+            <div className="van-card-img">
+                <img src={van.imageUrl} alt="" />
+            </div>
+            <div className="van-card-content">
+                <h3>{van.name}</h3>
+                <p><strong>${van.price}</strong> <small>/day</small></p>
+            </div>
+            <button className="van-type">
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </button>
+
+        </div>
+    ))
+
+
+
     return (
-        <h1>vans page</h1>
-    )
+        <div className="van-container">
+            <h2>Explore our van options</h2>
+            <div className="van-list">
+                {vanCard}
+            </div>
+        </div>
+    );
 }
